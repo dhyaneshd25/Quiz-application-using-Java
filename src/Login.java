d96cf611409773fdb3109db0a3b323d150b2ca04
Dhyanesh Janardan Dharmik <dharmikdj@rknec.edu>
2024-06-17 16:21:06 +0530

    file added...

--- src/Login.java
+++ src/Login.java
@@ -1,64 +1,69 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class login extends JFrame implements ActionListener{
-	
-	JButton playb,exitb;
-	JTextField inname;
-	
-	login(){
-		getContentPane().setBackground(Color.white);
-		setLayout(null);
-		ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/img1.jpg"));
-	    JLabel img1=new JLabel(i1);
-	    img1.setBounds(0,0,600,500);
-		add(img1);
-		
-		JLabel heading=new JLabel("Java Quiz");
-		heading.setBounds(750,60,300,45);
-		heading.setFont(new Font("Vinor Mand ITC",Font.BOLD,40));
-		add(heading);
-		
-		JLabel name=new JLabel("Enter Your Name");
-		name.setBounds(750,150,300,45);
-		name.setFont(new Font("Vinor Mand ITC",Font.BOLD,20));
-		add(name);
-		
-		inname=new JTextField();
-		inname.setBounds(750,240,300,45);
-		inname.setFont(new Font("Vinor Mand ITC",Font.BOLD,20));
-		add(inname);
-
-		playb =new JButton("Play");
-		playb.setBounds(750,300,100,45);
-		playb.setFont(new Font("Vinor Mand ITC",Font.BOLD,20));
-		playb.addActionListener(this);
-		add(playb);
-		
-		exitb =new JButton("Exit");
-		exitb.setBounds(870,300,100,45);
-		exitb.setFont(new Font("Vinor Mand ITC",Font.BOLD,20));
-		exitb.addActionListener(this);
-		add(exitb);
-
-		setSize(1200,500);
-		setLocation(100,100);
-		setVisible(true);
-	}
-	
-	public void actionPerformed(ActionEvent ae) {
-		if(ae.getSource()==playb) {
-			setVisible(false);
-			String n=inname.getText();
-			new playwindow(n);
-		}else if(ae.getSource()==exitb) {
-			setVisible(false);
-		}
-	}
-	public static void main(String args[]) {
-		
-		new login();
-		
-	}
+public class Login extends JFrame implements ActionListener{
+ 
+    JButton rules, back;
+    JTextField tfname;
+    
+    Login() {
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/img1.jpg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 600, 500);
+        add(image);
+        
+        JLabel heading = new JLabel(" EASY JAVA QUIZ");
+        heading.setBounds(750, 60, 300, 45);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel name = new JLabel("Enter Your Name");
+        name.setBounds(810, 150, 200, 30);
+        name.setFont(new Font("Mongolian Baiti", Font.BOLD, 20));
+        name.setForeground(new Color(30, 144, 254));
+        add(name);
+        
+        tfname = new JTextField();
+        tfname.setBounds(735, 200, 300, 25);
+        tfname.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(tfname);
+        
+        rules = new JButton("Play");
+        rules.setBounds(735, 270, 120, 25);
+        rules.setBackground(new Color(30, 144, 254));
+        rules.setForeground(Color.WHITE);
+        rules.addActionListener(this);
+        add(rules);
+        
+        back = new JButton("Exit");
+        back.setBounds(915, 270, 120, 25);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        setSize(1200, 500);
+        setLocation(100, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == rules) {
+            String name = tfname.getText();
+            setVisible(false);
+            new Rules(name);
+        } else if (ae.getSource() == back) {
+            setVisible(false);
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Login();
+    }
 }
